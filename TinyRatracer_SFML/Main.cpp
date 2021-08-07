620f1752ff16fcbe1cbc32c8b5968e0062552ca8
DESKTOP-3JJ969J\diskhkme <diskhkme@gmail.com>
2021-08-07 17:46:33 +0900

    texture generation and display test

--- TinyRatracer_SFML/Main.cpp
+++ TinyRatracer_SFML/Main.cpp
@@ -2,9 +2,39 @@
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	constexpr int WIDTH = 1024;
+	constexpr int HEIGHT = 768;
+
+	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "SFML works!");
+	window.setVerticalSyncEnabled(true);
+
+	sf::Texture targetTexture;
+	if (!targetTexture.create(WIDTH, HEIGHT))
+	{
+		return -1;
+	}
+
+	sf::Uint8* pixels = new sf::Uint8[WIDTH * HEIGHT * 4];
+
+	sf::Clock clock;
+	for (size_t j = 0; j < HEIGHT; j++)
+	{
+		for (size_t i = 0; i < WIDTH; i++)
+		{
+			pixels[(i + j * WIDTH) * 4 + 0] = static_cast<sf::Uint8>((j / float(HEIGHT))*255); // R Channel
+			pixels[(i + j * WIDTH) * 4 + 1] = static_cast<sf::Uint8>((i / float(WIDTH))*255); // G Channel
+			pixels[(i + j * WIDTH) * 4 + 2] = 0; // B Channel
+			pixels[(i + j * WIDTH) * 4 + 3] = 255; // A Channel
+		}
+	}
+	sf::Int32 elapsedTime = clock.getElapsedTime().asMilliseconds();
+	window.setTitle(sf::String{ std::to_string(elapsedTime) + " ms" });
+
+
+	targetTexture.update(pixels);
+
+	sf::Sprite sprite;
+	sprite.setTexture(targetTexture);
 
 	while (window.isOpen())
 	{
@@ -16,7 +46,7 @@ int main()
 		}
 
 		window.clear();
-		window.draw(shape);
+		window.draw(sprite);
 		window.display();
 	}
 
